812a82153d81dfbc6eb2425ce604a9d803f0cd89
Jim Hansen <kermijim@gmail.com>
2016-12-19 00:13:31 -0700

    Code Update

--- UltraSonicRangeSensor/UltrasonicRangeSensor.py
+++ UltraSonicRangeSensor/UltrasonicRangeSensor.py
@@ -11,30 +11,28 @@ print ("Distance Measurement in Progress")
 # Define functions -----------------------
 
 def measure():
-  GPIO.setmode(GPIO.BCM) # Use BCM GPIO references instead of physical pin numbers GPIO.setup(TRIG,GPIO.OUT)
-  GPIO.setup(ECHO,GPIO.IN)
-
   # This function measures a distance
-  GPIO.output(GPIO_TRIGGER, False)
-  
-  print ("Waiting For Sensor To Settle")
-  time.sleep(2)
-      
   GPIO.output(GPIO_TRIGGER, True)
   time.sleep(0.00001)
+  
   GPIO.output(GPIO_TRIGGER, False)
   start = time.time()
+
   while GPIO.input(GPIO_ECHO)==0:
     start = time.time()
+
   while GPIO.input(GPIO_ECHO)==1:
     stop = time.time()
+
   elapsed = stop-start
   distance = (elapsed * 34300)/2
-   
+
   return distance
 
-  def measure_average():
-   print ("This function takes 3 measurements and returns the average.")
+
+def measure_average():
+  # This function takes 3 measurements and
+  # returns the average.
   distance1=measure()
   time.sleep(0.1)
   distance2=measure()
@@ -44,29 +42,42 @@ def measure():
   distance = distance / 3
   return distance
 
-# ----------------------- Main Script ----------------------- Define GPIO to use on Pi
-GPIO.setup(TRIG,GPIO.OUT)
-GPIO.setup(ECHO,GPIO.IN)
 
+# -----------------------
+# Main Script
+# -----------------------
 
+# Use BCM GPIO references
+# instead of physical pin numbers
+GPIO.setmode(GPIO.BCM)
+
+# Define GPIO to use on Pi
 GPIO_TRIGGER = 7
-GPIO_ECHO = 12
+GPIO_ECHO    = 7
 
 print "Ultrasonic Measurement"
+
 # Set pins as output and input
-GPIO.setup(GPIO_TRIGGER,GPIO.OUT) # Trigger GPIO.setup(GPIO_ECHO,GPIO.IN) # Echo
+GPIO.setup(GPIO_TRIGGER,GPIO.OUT)  # Trigger
+GPIO.setup(GPIO_ECHO,GPIO.IN)      # Echo
+
 # Set trigger to False (Low)
 GPIO.output(GPIO_TRIGGER, False)
-# Wrap main content in a try block so we can catch the user pressing CTRL-C and run the GPIO cleanup function. This will also prevent the user 
-# seeing lots of unnecessary error messages.
-#try:
-#  while True:
-#
-#    distance = measure_average()
-#    print "Distance : %.1f" % distance
-#    time.sleep(1) 
-#    except KeyboardInterrupt:
-#
-    # User pressed CTRL-C Reset GPIO settings
-#
-GPIO.cleanup()
+
+# Wrap main content in a try block so we can
+# catch the user pressing CTRL-C and run the
+# GPIO cleanup function. This will also prevent
+# the user seeing lots of unnecessary error
+# messages.
+try:
+
+  while True:
+
+    distance = measure_average()
+    print "Distance : %.1f" % distance
+    time.sleep(1)
+
+except KeyboardInterrupt:
+  # User pressed CTRL-C
+  # Reset GPIO settings
+  GPIO.cleanup()
